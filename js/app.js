eab4f5e3c09329d69f30dd86d5d02eee63d251ed
Karla Nava <karla.nm8@gmail.com>
2018-01-05 14:53:59 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var jedis = [
+	{
+    "name":"Jonh",
+    "age":31,
+    "cars":[],
+    "preferences":[ "No, i love the choppers"]
+    },
+	{
+    "name":"Karla",
+    "age":25,
+    "cars":[ "Mini cooper", "Volvo C30", "BMW X6", "Fiat 500"],
+    "preferences":["i love cars"]
+    },
+	{
+    "name":"Amalia",
+    "age":26,
+    "cars":[ "Mustang", "BMW", "Beetle"],
+    "preferences":["i love cars"]
+    },
+	{
+    "name":"Silvana",
+    "age":31,
+    "cars":[ "Seat", "Golf", "Chevy", "Bocho"],
+    "preferences":["Yeah, but prefer the bicycles"]
+    },
+	{
+    "name":"Emmanuel",
+    "age":25,
+    "cars":[],
+    "preferences":["No, i don´t like. I love the sport motorcycles" ]
+    }
+];
+
+// 1. Elabora una función que retorne a los Jedis
+// 2. Elabora una función para filtrar por nombre, por edad y marca o modelo.
+// 3. Elabora una función que de el promedio de las edades de los Jedis
+// 4. Elabora una función que enliste todas las marcas o modelos de coches.
